fad07f301830346dec4ec4a4f4c0b3ec6547d449
Clang_error <wndwnddoq@gmail.com>
2026-09-01 16:14:44 +0900

    스윙 프레임 추가

---
+++ 9장/test/src/ch09/MyFrame.java
@@ -0,0 +1,17 @@
+package ch09;
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame{
+    Container c = getContentPane();
+    public MyFrame() {
+        setTitle("300x300 스윙 프레임 만들기");
+        setSize(300,300);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+    }
+}
