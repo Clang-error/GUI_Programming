1c9357455f8d6c96976792a7ab266cf40f50993b
Clang_error <wndwnddoq@gmail.com>
2026-09-01 15:21:11 +0900

    GUI 기초 실습

--- 9장/test/src/ch09/NullLayoutEx.java
+++ 9장/test/src/ch09/NullLayoutEx.java
@@ -13,14 +13,31 @@ public class NullLayoutEx extends JFrame{
         setTitle("First Frame");
         setSize(700,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //기본값
+        setLocationRelativeTo(null);
         setLayout(null); //이걸주면 지정이가능해짐 add를 해도 씹고 내가 지정
+
         JLabel label = new JLabel("Hello GUI");
-        label.setLocation(300,200); //이거 조절해도 add()하면 그냥 붙여버림
+        label.setLocation(300,50); //이거 조절해도 add()하면 그냥 붙여버림
         label.setSize(100,100); //위치 지정
         label.setFont(new Font(" ", Font.BOLD,24));
+        label.setOpaque(true);
+        label.setBackground(Color.red);
+        label.setForeground(Color.blue);
+
+        JButton btn = new JButton(" SAVE ");
+        btn.setLocation(200,150); //이거 조절해도 add()하면 그냥 붙여버림
+        btn.setSize(150,40); //위치 지정
+        btn.setFont(new Font(" ", Font.BOLD,24));
+
+        JButton btn2 = new JButton(" CANCEL ");
+        btn2.setLocation(350,150); //이거 조절해도 add()하면 그냥 붙여버림
+        btn2.setSize(150,40); //위치 지정
+        btn2.setFont(new Font(" ", Font.BOLD,24));
+
+
         add(label);
-
-
+        add(btn);
+        add(btn2);
         setVisible(true);
 
         System.out.println("hello world");
