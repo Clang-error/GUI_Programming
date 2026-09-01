1a1fd26705d5f98487f976cce4937b7eadcc0341
Clang_error <wndwnddoq@gmail.com>
2026-09-01 15:03:56 +0900

    Add ch09 NullLayoutEx

---
+++ 9장/test/src/ch09/NullLayoutEx.java
@@ -0,0 +1,28 @@
+package ch09;
+import java.awt.*;
+import javax.swing.*;
+//set title set size set Default... setLayout은 기본 후 레이블 생성
+// label을 출력하려면 어디에 출력할것인지 알려줘야한 x,y 그리고 로케이션
+
+public class NullLayoutEx extends JFrame{
+    public static void main(String[] args) {
+        new NullLayoutEx();
+    }
+
+    public NullLayoutEx() {
+        setTitle("First Frame");
+        setSize(700,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //기본값
+        setLayout(null); //이걸주면 지정이가능해짐 add를 해도 씹고 내가 지정
+        JLabel label = new JLabel("Hello GUI");
+        label.setLocation(300,200); //이거 조절해도 add()하면 그냥 붙여버림
+        label.setSize(100,100); //위치 지정
+        label.setFont(new Font(" ", Font.BOLD,24));
+        add(label);
+
+
+        setVisible(true);
+
+        System.out.println("hello world");
+    }
+}
