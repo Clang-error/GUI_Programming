2d9acc642c626475e9c72264d2dc367b07c3e273
Clang_error <wndwnddoq@gmail.com>
2026-09-03 14:54:53 +0900

    이름 입력 필드 추가, 입력기 강제 확정 로직, 왼쪽하단 이름 표시

--- Report01/Report/src/Ch09/MyGUI.java
+++ Report01/Report/src/Ch09/MyGUI.java
@@ -1,6 +1,8 @@
 package Ch09;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.util.Objects;
 import java.util.random.*;
 
@@ -29,15 +31,66 @@ public class MyGUI extends JFrame {
 
 
         //현재rgb값 라벨
-        Color test = c.getBackground();
+        Color startBackGround = c.getBackground();
         JLabel cRgb= new JLabel("");
         //줄넘김하려면 Html 형식 사용해야함 JLabel은 기본적으로 줄넘김을 지원하지않음
-        cRgb.setText("<html>현재 배경<br>R: "+test.getRed()+" G: "+test.getGreen()+" B: "+test.getBlue()+"</html>");
+        cRgb.setText("<html>현재 배경<br>R: "+startBackGround.getRed()+" G: "+startBackGround.getGreen()+" B: "+startBackGround.getBlue()+"</html>");
         cRgb.setBounds(580,590,200,100);
         cRgb.setFont(new Font("",Font.BOLD,12));
         cRgb.setForeground(Color.WHITE);
 
-        //텍스트필드
+        //현재 사용자 표시 라벨
+        JLabel myNamelbl=new JLabel("Unknown님 안녕하세요!");
+        myNamelbl.setBounds(20,600,200,100);
+        myNamelbl.setFont(new Font("",Font.BOLD,12));
+        myNamelbl.setForeground(Color.WHITE);
+
+
+        //이름 입력 텍스트필드
+        JTextField nameTf = new JTextField("이름을 입력하세요");
+        nameTf.setHorizontalAlignment(SwingConstants.CENTER);
+        nameTf.setBounds(55,50,600,50);
+        nameTf.setFont(new Font("",Font.BOLD,24));
+
+        nameTf.addFocusListener(new FocusAdapter() { //포커스 리스너, 클릭하면 채워져있던 텍스필드 지고 공백만남ㅜㅁ
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(nameTf.getText().equals("이름을 입력하세요"))
+                    nameTf.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) { //포커스가 딴곳으로 갔을때 필드를 다시채움
+                if(nameTf.getText().isEmpty()) {
+                    nameTf.setText("이름을 입력하세요");
+                }
+            }
+        });
+
+        //이름입력 제출버튼
+        JButton nameSubmitBtn = new JButton("제출");
+        nameSubmitBtn.setBounds(550,100,100,50);
+
+        nameSubmitBtn.addActionListener(e-> {
+            if(nameTf.getInputContext() != null) {
+                //조합중인 입력에서 끝나지않으면 버튼이 눌리지않는 버그발생
+                //강제 확정시켜서 씹기
+                nameTf.getInputContext().endComposition();
+            }
+            if(Objects.equals("이름을 입력하세요",nameTf.getText())) {
+                JOptionPane.showMessageDialog(null,"이름을 입력하시고 제출하세요"); //이름을 입력받게
+            }
+            else {//예 아니오 확인받고 출력하는 로직
+            int result = JOptionPane.showConfirmDialog(null,nameTf.getText() +"님 맞나요?","확인",JOptionPane.OK_CANCEL_OPTION);
+            if(result == JOptionPane.OK_OPTION) {
+                JOptionPane.showMessageDialog(null,nameTf.getText()+"님 안녕하세요!");
+                myNamelbl.setText(nameTf.getText()+"님 안녕하세요!");}
+            }
+        });
+
+
+
+        //색상변경 텍스트필드
         JTextField tf = new JTextField("");
         tf.setBounds(210,0,200,50);
         tf.setFont(new Font("",Font.BOLD,24));
@@ -77,7 +130,6 @@ public class MyGUI extends JFrame {
             int g = (int)(Math.random()*255)+1;
             int b = (int)(Math.random()*255)+1;
             lbl.setBackground(new Color(r,g,b));
-            Color current = c.getBackground();
         });
 
         //배경색 변경 버튼
@@ -121,6 +173,7 @@ public class MyGUI extends JFrame {
         rscl.setMinimum(1); //최솟값 최대값 지정
         rscl.setMaximum(256);
         rscl.setVisibleAmount(1);
+        rscl.setValue(startBackGround.getRed());
 
         rscl.addAdjustmentListener(e->{
             if (rOneTimeNoti) { //한번만 알림
@@ -139,6 +192,7 @@ public class MyGUI extends JFrame {
         lscl.setMinimum(1);
         lscl.setMaximum(256);
         lscl.setVisibleAmount(1);
+        lscl.setValue(startBackGround.getGreen());
 
         lscl.addAdjustmentListener(e->{
             if (lOneTimeNoti) { //한번만 알림
@@ -157,6 +211,7 @@ public class MyGUI extends JFrame {
         bscl.setMinimum(1);
         bscl.setMaximum(256);
         bscl.setVisibleAmount(1);
+        bscl.setValue(startBackGround.getBlue());
 
         bscl.addAdjustmentListener(e->{
             if(bOneTimeNoti) {
@@ -166,7 +221,7 @@ public class MyGUI extends JFrame {
             int b = e.getValue();
             Color current = c.getBackground();
             c.setBackground(new Color(current.getRed(),current.getGreen(),b));
-            cRgb.setText("R: "+current.getRed()+" G: "+current.getGreen()+" B: "+current.getBlue() );
+            cRgb.setText("<html>현재 배경<br>R: "+current.getRed()+" G: "+current.getGreen()+" B: "+current.getBlue()+"</html>");
         });
 
         //랜덤버튼
@@ -192,12 +247,17 @@ public class MyGUI extends JFrame {
             tf.setBackground(Color.WHITE);
             Color current = c.getBackground();
             cRgb.setText("<html>현재 배경<br>R: "+current.getRed()+" G: "+current.getGreen()+" B: "+current.getBlue()+"</html>");
+            myNamelbl.setText("Unknown님 안녕하세요!");
+            nameTf.setText("이름을 입력하세요");
         });
 
         setLocationRelativeTo(null); //실행시 gui를 화면 중앙에 배치
         c.add(lbl);
         c.add(lblHide);
         c.add(lblShow);
+        c.add(myNamelbl);
+        c.add(nameSubmitBtn);
+        c.add(nameTf);
         c.add(cRgb);
         c.add(btn1);
         c.add(btn2);
