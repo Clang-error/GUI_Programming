31bd7a5d4013d8f155ccadaa3ea7f7a46afb3cd9
Clang_error <wndwnddoq@gmail.com>
2026-09-12 00:41:48 +0900

    주석 추가

--- Report02/Report/src/ch09/GridLayoutEx.java
+++ Report02/Report/src/ch09/GridLayoutEx.java
@@ -10,10 +10,15 @@ public class GridLayoutEx extends JFrame {
         setTitle("GridLayout test");
         setSize(300,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        c.setLayout(new GridLayout());
 
+        //그리드 레이아웃 사용
+        //row를 5로 지정하면 정사각형 그리드가 나옴
+        //row를 0 cols 7로하면 버튼이 7개가 한 열에 나옴
+        c.setLayout(new GridLayout(5,5));
+
+        //버튼 생성및 추가
         for(int i=0; i<btn.length;i++) {
-            btn[i]=new JButton(i+"");
+            btn[i]=new JButton((i+1)+"");
             c.add(btn[i]);
         }
 
